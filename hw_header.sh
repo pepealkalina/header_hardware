a5e4452ec9a2c9db823ca1dd57242c449acbfbce
pepealkalina <111188050+pepealkalina@users.noreply.github.com>
2025-01-21 13:31:57 +0100

    Create hw_header.sh
    añade el fichero hw_header

---
+++ hw_header.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Creador: Pedro Reina
+# Fecha Creacion: 21-01-2025
+
+# Crea un archivo cuyo nombre se pasa por parametro y le añade un header
+
+if [ $# -lt 1 ]
+    then
+        echo "Error: Porfavor introduce un archivo"
+        exit 1
+    fi
+
+# Cambiar dependiendo del alumno
+alumno="Pedro Reina"
+
+
+echo "Creando archivo y añadiendo el header a $1"
+
+echo '#!/bin/bash' > $1
+echo "" >> $1s
+echo "# Alumno: $alumno" >> $1
+echo "# Fecha Creacion: $(date)" >> $1
+
+read -p "Introduce una breve descripcion del programa: " Descripcion
+
+echo "# Descripcion: $Descripcion" >> $1
